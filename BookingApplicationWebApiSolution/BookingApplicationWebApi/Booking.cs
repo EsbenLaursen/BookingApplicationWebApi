cd15cdfc94f025c4b090d8090539501777857e94
EsbenLaursen <Esben.laursen@gmail.com>
2016-11-20 15:33:12 +0100

    Entity

---
+++ BookingApplicationWebApiSolution/BookingApplicationWebApi/Booking.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookingApplicationWebApi
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+    }
+}
