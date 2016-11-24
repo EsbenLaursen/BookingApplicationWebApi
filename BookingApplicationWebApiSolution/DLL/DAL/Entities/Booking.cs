a0d9b8a005e5e99c6da62e7b9738cfb4eaa3afb4
Emildall <emildall.ed@gmail.com>
2016-11-24 10:35:07 +0100

    entities added

---
+++ BookingApplicationWebApiSolution/DLL/DAL/Entities/Booking.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DLL.DAL.Entities;
+
+namespace BookingApplicationWebApi
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public Customer Customer { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Room Room { get; set; }
+        public bool Breakfast { get; set; }
+
+    }
+}
---
+++ BookingApplicationWebApiSolution/DLL/DAL/Entities/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.DAL.Entities
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string PhoneNr { get; set; }
+    }
+}
