ecfa982d9567b83258f666ece0403f3c3c330d5a
EsbenLaursen <Esben.laursen@gmail.com>
2016-11-27 14:00:13 +0100

    Added customer tests (CRUD)

---
+++ BookingApplicationWebApiSolution/DLL/Repositories/CustomerRepository.cs
@@ -0,0 +1,37 @@
+﻿using DLL.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.Repositories
+{
+    public class CustomerRepository : IRepository<Customer>
+    {
+        public Customer Create(Customer t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Customer t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Customer t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
