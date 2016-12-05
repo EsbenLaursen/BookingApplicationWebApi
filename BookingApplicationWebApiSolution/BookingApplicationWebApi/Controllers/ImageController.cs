934d0e37f2e24b721a5faf3ff25a335de16ea2e4
Emildall <emildall.ed@gmail.com>
2016-12-05 10:40:38 +0100

    Updated Image classes, not done. Added hardcoded footcare in dbiniti

--- BookingApplicationWebApiSolution/BookingApplicationWebApi/Controllers/ImageController.cs
+++ BookingApplicationWebApiSolution/BookingApplicationWebApi/Controllers/ImageController.cs
@@ -1,77 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity.Migrations;
-using System.Data.Entity.Validation;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using DLL;
 using DLL.DAL;
 using DLL.DAL.Entities;
-using DLL.Repositories;
-using WebGrease.Css.Extensions;
 
 namespace BookingApplicationWebApi.Controllers
 {
     public class ImageController : Controller
     {
-        private IRepository<Image> repo = new DllFacade().GetImageManager();
-
         // GET: Image
-        public ActionResult Index()
+        public ActionResult UploadImage()
         {
             return View();
         }
-
-        public ActionResult Gallery()
-        {
-           
-            List<Image> all = new List<Image>();
-             //Chooses the BookingDbContext as our datacontext
-                using (BookingDbContext dc = new BookingDbContext())
-                {
-                    all = dc.Images.ToList();
-                 }
-            
-
-            return View(all);
-        }
-
-     
-        public ActionResult Upload()
-        {
-            return View();
-        }
-
 
         [HttpPost]
-        public ActionResult Upload(Image IG)
+        public ActionResult Upload(HttpPostedFileBase image, Image photo)
         {
-          
-            
-            if (IG.File.ContentLength > (2*1024*1024))
+            var path = "";
+
+            if (image != null)
             {
-                ModelState.AddModelError("CustomError", "File size must be less than 2 MEGAMEGABITSZ");
-                return View();
+                if (image.ContentLength > 0)
+                {
+                    if (Path.GetExtension(image.FileName).ToLower().Contains("jpg") ||
+                        Path.GetExtension(image.FileName).ToLower().Contains("gif") ||
+                        Path.GetExtension(image.FileName).ToLower().Contains("gif") ||
+                        Path.GetExtension(image.FileName).ToLower().Contains("jpeg"))
+                    {
+                        photo.ImageFileName = image.FileName;
+                        photo.ImageMimeType = image.ContentType;
+                        photo.PhotoFile = new byte[image.ContentLength];
+                        image.InputStream.Read(photo.PhotoFile, 0, image.ContentLength);
+                    }
+                    path = Path.Combine(Server.MapPath("~/Content/Images"), image.FileName);
+                    BookingDbContext db = new BookingDbContext();
+
+                    db.Images.Add(photo);
+                    db.SaveChanges();
+                    ViewBag.UploadSuccess = true;
+                    return RedirectToAction("Upload");
+                }
             }
-
-            if (!(IG.File.ContentType == "image/jpeg" || IG.File.ContentType == "image/png"))
-            {
-                ModelState.AddModelError("CustomError", "File type must be Jpeg or png");
-                return View();
-            }
-
-            IG.FileName = IG.File.FileName;
-            IG.ImageSize = IG.File.ContentLength;
-            
-
-            byte[] data = new byte[IG.File.ContentLength];
-            IG.File.InputStream.Read(data, 0, IG.File.ContentLength);
-                
-            repo.Create(IG);
-
-            return RedirectToAction("Gallery");
+            return View(image);
         }
-
     }
 }
---
+++ BookingApplicationWebApiSolution/BookingApplicationWebApi/Models/ImageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DLL.DAL.Entities;
+
+namespace BookingApplicationWebApi.Models
+{
+    public class ImageViewModel
+    {
+        public List<Image> Images { get; set; }
+    }
+}
