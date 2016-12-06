1b95123f3359d3d5b636929784572dc9729ebc80
Emildall <emildall.ed@gmail.com>
2016-12-06 10:08:19 +0100

    Fixed image test class

--- BookingApplicationWebApiSolution/UnitTestS/ImageRepositoryTest.cs
+++ BookingApplicationWebApiSolution/UnitTestS/ImageRepositoryTest.cs
@@ -1,284 +1,283 @@
-﻿//using System;
-//using System.Text;
-//using System.Collections.Generic;
-//using System.Linq;
-//using DLL.DAL.Entities;
-//using DLL.DAL.Managers;
-//using DLL.DAL.Repositories;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using Moq;
-
-//namespace UnitTestS
-//{
-    
-//    [TestClass]
-//    public class ImageRepositoryTest
-//    {
-//        private static Mock<IRepository<Image>> mock;
-
-//        // Internal list of Images replacing the repository (fake repository)
-//        private static IList<Image> images = new List<Image>();
-
-//        [ClassInitialize]
-
-//        public static void classinititializer(TestContext context)
-//        {
-
-//            mock = new Mock<IRepository<Image>>();
-//            mock.Setup(x => x.Create(It.IsAny<Image>())).Callback<Image>((s) => images.Add(s));
-//            mock.Setup(x => x.ReadAll()).Returns(() => images.ToList());
-//            mock.Setup(x => x.Read(It.IsAny<int>())).Returns((int id) => images.FirstOrDefault(x => x.ImageId == id));
-//            mock.Setup(x => x.Delete(It.IsAny<Image>())).Callback<Image>((s) => images.Remove(s));
-//            mock.Setup(x => x.Update(It.IsAny<Image>()))
-//                .Callback<Image>((s) => images.Insert(images.IndexOf(images.FirstOrDefault(x => x.ImageId == s.ImageId)), s));
-//        }
-
-//        /// <summary>
-//        /// Executed before each test method is executed.
-//        /// Ensures each test is executed on an empty repository.
-//        /// </summary>
-
-//        [TestInitialize]
-//        public void testInitializer()
-//        {
-
-//            images.Clear();
-//        }
-
-//        /// <summary>
-//        /// Test method testing the creation of a ImageManager with an existing repository.
-//        /// </summary>
-
-//        [TestMethod]
-//        public void CreateImageManagerExitingRepositoryTest()
-//        {
-//            IRepository<Image> repo = mock.Object;
-//            ImageManager im = new ImageManager(repo);
-
-//            Assert.IsNotNull(im);
-//            Assert.AreEqual(0, images.Count);
-//        }
-
-//        /// <summary>
-//        /// Test method testing creation of a ImageManager with no repository (null).
-//        /// Expects ArgumentNullException to be thrown.
-//        /// </summary>
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using DLL.DAL.Entities;
+using DLL.DAL.Managers;
+using DLL.DAL.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTestS
+{
+
+    [TestClass]
+    public class ImageRepositoryTest
+    {
+        private static Mock<IRepository<Image>> mock;
+
+        // Internal list of Images replacing the repository (fake repository)
+        private static IList<Image> images = new List<Image>();
+
+        [ClassInitialize]
+
+        public static void classinititializer(TestContext context)
+        {
+
+            mock = new Mock<IRepository<Image>>();
+            mock.Setup(x => x.Create(It.IsAny<Image>())).Callback<Image>((s) => images.Add(s));
+            mock.Setup(x => x.ReadAll()).Returns(() => images.ToList());
+            mock.Setup(x => x.Read(It.IsAny<int>())).Returns((int id) => images.FirstOrDefault(x => x.ImageId == id));
+            mock.Setup(x => x.Delete(It.IsAny<Image>())).Callback<Image>((s) => images.Remove(s));
+            mock.Setup(x => x.Update(It.IsAny<Image>()))
+                .Callback<Image>((s) => images.Insert(images.IndexOf(images.FirstOrDefault(x => x.ImageId == s.ImageId)), s));
+        }
+
+        /// <summary>
+        /// Executed before each test method is executed.
+        /// Ensures each test is executed on an empty repository.
+        /// </summary>
+
+        [TestInitialize]
+        public void testInitializer()
+        {
+
+            images.Clear();
+        }
+
+        /// <summary>
+        /// Test method testing the creation of a ImageManager with an existing repository.
+        /// </summary>
 
-//        [TestMethod]
-//        [ExpectedException(typeof(ArgumentNullException))]
-//        public void CreateImageManagerNoRepositoryExceptionExcpected()
-//        {
-//            IRepository<Image> repo = null;
-//            ImageManager im = new ImageManager(repo);
+        [TestMethod]
+        public void CreateImageManagerExitingRepositoryTest()
+        {
+            IRepository<Image> repo = mock.Object;
+            ImageManager im = new ImageManager(repo);
 
-
-//            Assert.Fail("Created ImageManager with null repository");
-//        }
-
-//        /// <summary>
-//        /// Test method testing adding a new Image to the repository.
-//        /// </summary>
-
-//        [TestMethod]
-//        public void AddNewImageTest()
-//        {
-//            IRepository< Image > repo = mock.Object;
-//            ImageManager im = new ImageManager(repo);
-
-
-//            Image i = new Image() { ImageId = 1, ImageFileName = "Image" };
-//            im.Create(i);
-
-//            Assert.AreEqual(i, im.Read(1));
-//            Assert.AreEqual(1, images.Count);
-
-//        }
-
-//        /// <summary>
-//        /// Test method adding an existing image to the repository.
-//        /// Expects an ArgumentException to be thrown.
-//        /// </summary>
-
-//        [TestMethod]
-//        public void AddImageExistingImageTest()
-//        {
-//            IRepository< Image > repo = mock.Object;
-//            ImageManager im = new ImageManager(repo);
+            Assert.IsNotNull(im);
+            Assert.AreEqual(0, images.Count);
+        }
 
+        /// <summary>
+        /// Test method testing creation of a ImageManager with no repository (null).
+        /// Expects ArgumentNullException to be thrown.
+        /// </summary>
 
-//            Image i = new Image() { ImageId = 1, ImageFileName = "Image" };
-//            im.Create(i);
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CreateImageManagerNoRepositoryExceptionExcpected()
+        {
+            IRepository<Image> repo = null;
+            ImageManager im = new ImageManager(repo);
 
-//            try
-//            {
-//                im.Create(i); // try to add the same image again
-//                Assert.Fail("Added existing image to repository");
-//            }
-//            catch (ArgumentException)
-//            {
-//                Assert.AreEqual(1, images.Count);
-//                Assert.AreEqual(i, im.Read(1));
-//            }
-//        }
 
-//        /// <summary>
-//        /// Test method testing removal of an existing Image.
-//        /// </summary>
+            Assert.Fail("Created ImageManager with null repository");
+        }
 
-//        [TestMethod]
-//        public void RemoveImageExistingImageTest()
-//        {
-//            IRepository<Image> repo = mock.Object;
-//            ImageManager im = new ImageManager(repo);
+        /// <summary>
+        /// Test method testing adding a new Image to the repository.
+        /// </summary>
+
+        [TestMethod]
+        public void AddNewImageTest()
+        {
+            IRepository<Image> repo = mock.Object;
+            ImageManager im = new ImageManager(repo);
+
+
+            Image i = new Image() { ImageId = 1 };
+            im.Create(i);
+
+            Assert.AreEqual(i, im.Read(1));
+            Assert.AreEqual(1, images.Count);
+
+        }
 
+        /// <summary>
+        /// Test method adding an existing image to the repository.
+        /// Expects an ArgumentException to be thrown.
+        /// </summary>
 
-//            Image i = new Image() { ImageId = 1, ImageFileName = "Image" };
+        [TestMethod]
+        public void AddImageExistingImageTest()
+        {
+            IRepository<Image> repo = mock.Object;
+            ImageManager im = new ImageManager(repo);
 
-//            Image ii = new Image() { ImageId = 2, ImageFileName = "Image" };
 
-//            im.Create(i);
-//            im.Create(ii);
+            Image i = new Image() { ImageId = 1 };
+            im.Create(i);
 
-//            im.Delete(i);
+            try
+            {
+                im.Create(i); // try to add the same image again
+                Assert.Fail("Added existing image to repository");
+            }
+            catch (ArgumentException)
+            {
+                Assert.AreEqual(1, images.Count);
+                Assert.AreEqual(i, im.Read(1));
+            }
+        }
 
-//            Assert.AreEqual(1, images.Count);
-//            Assert.AreEqual(ii, im.ReadAll()[0]);
+        /// <summary>
+        /// Test method testing removal of an existing Image.
+        /// </summary>
 
+        [TestMethod]
+        public void RemoveImageExistingImageTest()
+        {
+            IRepository<Image> repo = mock.Object;
+            ImageManager im = new ImageManager(repo);
 
-//        }
 
-//        /// <summary>
-//        /// Test method testing removal of a non-existing Image.
-//        /// Expects an ArgumentException to be thrown.
-//        /// </summary>
+            Image i = new Image() { ImageId = 1 };
 
-//        [TestMethod]
-//        public void RemoveImageNonExistingImageTest()
-//        {
-//            IRepository<Image> repo = mock.Object;
-//            ImageManager im = new ImageManager(repo);
+            Image ii = new Image() { ImageId = 2};
 
+            im.Create(i);
+            im.Create(ii);
 
-//            Image i = new Image() { ImageId = 1, ImageFileName = "Image" };
+            im.Delete(i);
 
-//            Image ii = new Image() { ImageId = 2, ImageFileName = "Image" };
+            Assert.AreEqual(1, images.Count);
+            Assert.AreEqual(ii, im.ReadAll()[0]);
 
-//            im.Create(i);
 
-//            try
-//            {
-//                im.Delete(ii);
-//                Assert.Fail("Removed none existing Image ");
-//            }
-//            catch (ArgumentException)
-//            {
-//                Assert.AreEqual(1, images.Count);
-//                Assert.AreEqual(i, im.ReadAll()[0]);
-//            }
-//        }
-//        /// <summary>
-//        /// Test method testing the retrieval of all images from the repository.
-//        /// </summary>
-//        [TestMethod]
-//        public void ReadAllImages_Test()
-//        {
-//            IRepository<Image> repo = mock.Object;
-//            ImageManager im = new ImageManager(repo);
+        }
 
+        /// <summary>
+        /// Test method testing removal of a non-existing Image.
+        /// Expects an ArgumentException to be thrown.
+        /// </summary>
 
-//            Image i = new Image() { ImageId = 1, ImageFileName = "Image" };
+        [TestMethod]
+        public void RemoveImageNonExistingImageTest()
+        {
+            IRepository<Image> repo = mock.Object;
+            ImageManager im = new ImageManager(repo);
 
-//            Image ii = new Image() { ImageId = 2, ImageFileName = "Image" };
 
-//            im.Create(i);
-//            im.Create(ii);
+            Image i = new Image() { ImageId = 1 };
 
-//            IList<Image> result = im.ReadAll();
+            Image ii = new Image() { ImageId = 2 };
 
-//            Assert.AreEqual(2, result.Count);
-//            Assert.AreEqual(i, result[0]);
-//            Assert.AreEqual(ii, result[1]);
+            im.Create(i);
 
-//        }
-//        /// <summary>
-//        /// Test method testing retrieval of an existing image with a specific Id.
-//        /// </summary>
+            try
+            {
+                im.Delete(ii);
+                Assert.Fail("Removed none existing Image ");
+            }
+            catch (ArgumentException)
+            {
+                Assert.AreEqual(1, images.Count);
+                Assert.AreEqual(i, im.ReadAll()[0]);
+            }
+        }
+        /// <summary>
+        /// Test method testing the retrieval of all images from the repository.
+        /// </summary>
+        [TestMethod]
+        public void ReadAllImages_Test()
+        {
+            IRepository<Image> repo = mock.Object;
+            ImageManager im = new ImageManager(repo);
 
-//        [TestMethod]
-//        public void GetImageById_Existing_Image_Test()
-//        {
-//            IRepository<Image> repo = mock.Object;
-//            ImageManager im = new ImageManager(repo);
 
+            Image i = new Image() { ImageId = 1 };
 
-//            Image i = new Image() { ImageId = 1, ImageFileName = "Image" };
+            Image ii = new Image() { ImageId = 2 };
 
-//            Image ii = new Image() { ImageId = 2, ImageFileName = "Image" };
+            im.Create(i);
+            im.Create(ii);
 
-//            im.Create(i);
-//            im.Create(ii);
+            IList<Image> result = im.ReadAll();
 
-//            Image result = im.Read(1);
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual(i, result[0]);
+            Assert.AreEqual(ii, result[1]);
 
-//            Assert.AreEqual(i, result);
-//        }
-//        /// <summary>
-//        /// Testing the updating of an existing Image
-//        /// </summary>
+        }
+        /// <summary>
+        /// Test method testing retrieval of an existing image with a specific Id.
+        /// </summary>
 
-//        [TestMethod]
-//        public void UpdateexistingImage()
-//        {
-//            IRepository<Image> repo = mock.Object;
-//            ImageManager im = new ImageManager(repo);
+        [TestMethod]
+        public void GetImageById_Existing_Image_Test()
+        {
+            IRepository<Image> repo = mock.Object;
+            ImageManager im = new ImageManager(repo);
 
-//            //Create and adds the room
-//            Image i = new Image() { ImageId = 1, ImageFileName = "Image" };
-//            im.Create(i);
 
-//            Image ii = new Image() { ImageId = 1, ImageFileName = "Image2" };
+            Image i = new Image() { ImageId = 1 };
 
-//            bool isUpdated = im.Update(ii);
+            Image ii = new Image() { ImageId = 2 };
 
-//            Assert.AreEqual(true, isUpdated);
-//            Assert.AreEqual(i.ImageFileName, ii.ImageFileName);
-//           Assert.AreEqual(i.ImageId, ii.ImageId);
-//        }
+            im.Create(i);
+            im.Create(ii);
 
-//        /// <summary>
-//        /// Test method testing the update of an Image that doesnt exist
-//        /// </summary>
+            Image result = im.Read(1);
 
-//        [TestMethod]
-//        [ExpectedException(typeof(ArgumentNullException))]
-//        public void UpdateImageThatDoesntexist()
-//        {
-//            IRepository<Image> repo = mock.Object;
-//            ImageManager im = new ImageManager(repo);
+            Assert.AreEqual(i, result);
+        }
+        /// <summary>
+        /// Testing the updating of an existing Image
+        /// </summary>
 
-//            //Create room
-//            Image i = new Image() { ImageId = 1, ImageFileName = "Image" };
-//            //try update the room which doesnt exist
-//            im.Update(i);
+        [TestMethod]
+        public void UpdateexistingImage()
+        {
+            IRepository<Image> repo = mock.Object;
+            ImageManager im = new ImageManager(repo);
 
-//            Assert.Fail("Updated Image that didnt exist");
-//        }
+            //Create and adds the room
+            Image i = new Image() { ImageId = 1 };
+            im.Create(i);
 
-//        /// <summary>
-//        /// Testing Image with null parameter
-//        /// </summary>
+            Image ii = new Image() { ImageId = 1 };
 
-//        [TestMethod]
-//        [ExpectedException(typeof(ArgumentNullException))]
-//        public void UpdateRoomWithNullParameter()
-//        {
-//            IRepository<Image> repo = mock.Object;
-//            ImageManager im = new ImageManager(repo);
+            bool isUpdated = im.Update(ii);
 
-//            im.Update(null);
+            Assert.AreEqual(true, isUpdated);
+            Assert.AreEqual(i.ImageId, ii.ImageId);
+        }
 
-//            Assert.Fail("Updated image with null");
-//        }
+        /// <summary>
+        /// Test method testing the update of an Image that doesnt exist
+        /// </summary>
 
-//    }
-//}
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void UpdateImageThatDoesntexist()
+        {
+            IRepository<Image> repo = mock.Object;
+            ImageManager im = new ImageManager(repo);
+
+            //Create room
+            Image i = new Image() { ImageId = 1 };
+            //try update the room which doesnt exist
+            im.Update(i);
+
+            Assert.Fail("Updated Image that didnt exist");
+        }
+
+        /// <summary>
+        /// Testing Image with null parameter
+        /// </summary>
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void UpdateRoomWithNullParameter()
+        {
+            IRepository<Image> repo = mock.Object;
+            ImageManager im = new ImageManager(repo);
+
+            im.Update(null);
+
+            Assert.Fail("Updated image with null");
+        }
+
+    }
+}
