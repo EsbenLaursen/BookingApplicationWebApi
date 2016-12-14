f748af57009e6448328174a0ed12ec3f587e78a6
Emildall <emildall.ed@gmail.com>
2016-12-14 12:16:33 +0100

    Added CheckRoomAvailability Test - All tests passed

--- BookingApplicationWebApiSolution/DLL/CheckRoomAvailability.cs
+++ BookingApplicationWebApiSolution/DLL/CheckRoomAvailability.cs
@@ -13,8 +13,8 @@ namespace DLL
     {
         public IRepository<Room> sg = new DllFacade().GetRoomManager();
         public IRepository<Booking> bg = new DllFacade().GetBookingManager();
-        List<Booking> Bookings;
-        List<Room> Rooms;
+        public List<Booking> Bookings;
+        public List<Room> Rooms;
         public CheckRoomAvailability()
         {
             Rooms = sg.ReadAll();
@@ -23,13 +23,18 @@ namespace DLL
 
         public List<Room> Check(DateTime start, DateTime end)
         {
+            List<Room> RoomsAvailable = new List<Room>();
+            if (start.Date >= DateTime.Now.Date)
+            {
+                
+            
             var dates = new List<DateTime>();
             for (var dt = start; dt <= end; dt = dt.AddDays(1))
             {
                 dates.Add(dt);
             }
 
-            List<Room> RoomsAvailable = new List<Room>();
+            
 
             foreach (var r1 in Rooms)//3rooms
             {
@@ -54,58 +59,10 @@ namespace DLL
                 {
                     RoomsAvailable.Add(r1);
                 }
-            }
+            }}
 
             return RoomsAvailable;
         }
-
-
-        public List<DateTime> FetchUnavailableDates2()
-        {
-            List<DateTime> room1 = new List<DateTime>();
-            List<DateTime> room2 = new List<DateTime>();
-            List<DateTime> room3 = new List<DateTime>();
-
-            List<DateTime> UnavailableDates = new List<DateTime>();
-
-            foreach (var b in Bookings)
-            {
-                foreach (var r in b.Room)
-                {
-                    if (r.Id == 1)
-                    {
-                        for (var dt = b.StartDate; dt <= b.EndDate; dt = dt.AddDays(1))
-                        {
-                            room1.Add(dt);
-                        }
-                    }
-                    else if (r.Id == 2)
-                    {
-                        for (var dt = b.StartDate; dt <= b.EndDate; dt = dt.AddDays(1))
-                        {
-                            room2.Add(dt);
-                        }
-                    }
-                    else
-                    {
-                        for (var dt = b.StartDate; dt <= b.EndDate; dt = dt.AddDays(1))
-                        {
-                            room3.Add(dt);
-                        }
-                    }
-                }
-            }
-
-            foreach (var d in room1)
-            {
-                if (room2.Any(x => x.Day == d.Day && x.Month == d.Month && x.Year == d.Year) && room3.Any(x => x.Day == d.Day && x.Month == d.Month && x.Year == d.Year))
-                {
-                    UnavailableDates.Add(d);
-                }
-            }
-            return UnavailableDates;
-        }
-
 
 
         public List<DateTime> FetchUnavailableDates()
---
+++ BookingApplicationWebApiSolution/UnitTestS/CheckRoomsAvailabilityTest.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using DLL;
+using DLL.DAL.Entities;
+using DLL.DAL.Managers;
+using DLL.DAL.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTestS
+{
+    [TestClass]
+    public class CheckRoomsAvailabilityTest
+    {
+        private static IList<Booking> bookings = new List<Booking>();
+        private static IList<Room> rooms = new List<Room>();
+
+
+        [TestInitialize]
+        public void TestInitializer()
+        {
+            bookings.Clear();
+            rooms.Clear();
+        }
+
+        /// <summary>
+        /// Check if you can book in the past
+        /// </summary>
+        [TestMethod]
+        public void CheckForPastDateParametersTest()
+        {
+            var rooms = new List<Room>();
+            var books = new List<Booking>();
+
+            var startdate = DateTime.Now;
+            var enddate = DateTime.Now;
+
+            var past = DateTime.Now.AddDays(-1);
+
+            Room r = new Room() { Id = 1, Name = "Carlo" };
+            Room rr = new Room() { Id = 2, Name = "Morten" };
+
+            rooms.Add(r);
+            rooms.Add(rr);
+
+            Booking b = new Booking() { Id = 1, Room = new List<Room> { r }, EndDate = enddate, StartDate = startdate };
+            books.Add(b);
+
+            var ava = new CheckRoomAvailability();
+            ava.Rooms = rooms;
+            ava.Bookings = books;
+
+            var availeblerooms = ava.Check(past, past);
+
+            Assert.AreEqual(0, availeblerooms.Count);
+
+        }
+        /// <summary>
+        /// Checks if there are any available rooms if there are 0 rooms
+        /// </summary>
+        [TestMethod]
+        public void CheckForWithoutRoomTest()
+        {
+            var rooms = new List<Room>();
+            var books = new List<Booking>();
+
+            var startdate = DateTime.Now;
+            var enddate = DateTime.Now;
+
+
+            Booking b = new Booking() { Id = 1, EndDate = enddate, StartDate = startdate };
+            books.Add(b);
+
+            var ava = new CheckRoomAvailability();
+            ava.Rooms = rooms;
+            ava.Bookings = books;
+
+
+            var list = ava.Check(startdate, enddate);
+
+            Assert.AreEqual(0, list.Count);
+
+        }
+        /// <summary>
+        /// Checks if there are no bookings all rooms can be booked
+        /// </summary>
+        [TestMethod]
+        public void CheckForWithoutBookingsTest()
+        {
+            var rooms = new List<Room>();
+            var books = new List<Booking>();
+
+            var startdate = DateTime.Now;
+            var enddate = DateTime.Now.AddDays(1);
+
+            var past = DateTime.Now.AddDays(-1);
+
+            Room r = new Room() { Id = 1, Name = "Carlo" };
+            Room rr = new Room() { Id = 2, Name = "Morten" };
+
+            rooms.Add(r);
+            rooms.Add(rr);
+
+
+            var ava = new CheckRoomAvailability();
+            ava.Rooms = rooms;
+            ava.Bookings = books;
+
+            var availeblerooms = ava.Check(startdate, enddate);
+
+            Assert.AreEqual(2, availeblerooms.Count);
+
+        }
+        /// <summary>
+        /// Checks if a room cannot be booked if theres is already a booking on the date
+        /// </summary>
+        [TestMethod]
+        public void CheckForOverlappingDatesTest()
+        {
+            var rooms = new List<Room>();
+            var books = new List<Booking>();
+
+            var startdate = DateTime.Now;
+            var enddate = DateTime.Now.AddDays(1);
+
+            Room r = new Room() { Id = 1, Name = "Carlo" };
+            Room rr = new Room() { Id = 2, Name = "Morten" };
+
+            rooms.Add(r);
+            rooms.Add(rr);
+
+            Booking b = new Booking() { Id = 1, Room = new List<Room> { r }, StartDate = startdate, EndDate = enddate };
+            r.Bookings = new List<Booking> { b };
+            books.Add(b);
+
+            var ava = new CheckRoomAvailability();
+            ava.Rooms = rooms;
+            ava.Bookings = books;
+
+            var availeblerooms = ava.Check(startdate, enddate);
+
+            Assert.AreEqual(1, availeblerooms.Count);
+            Assert.AreEqual(rr.Id, availeblerooms[0].Id);
+
+        }
+
+
+        /// <summary>
+        /// Checks if a room can be booked eventhough  there are bookings on other dates
+        /// </summary>
+        [TestMethod]
+        public void CheckForNonOverlappingDatesTest()
+        {
+            var rooms = new List<Room>();
+            var books = new List<Booking>();
+
+            var startdate = DateTime.Now;
+            var enddate = DateTime.Now.AddDays(1);
+
+            Room r = new Room() { Id = 1, Name = "Carlo" };
+            Room rr = new Room() { Id = 2, Name = "Morten" };
+
+            rooms.Add(r);
+            rooms.Add(rr);
+
+            Booking b = new Booking() { Id = 1, Room = new List<Room> { r }, StartDate = startdate.AddDays(5), EndDate = enddate.AddDays(5) };
+            r.Bookings = new List<Booking> { b };
+            books.Add(b);
+
+            var ava = new CheckRoomAvailability();
+            ava.Rooms = rooms;
+            ava.Bookings = books;
+
+            var availeblerooms = ava.Check(startdate, enddate);
+
+            Assert.AreEqual(2, availeblerooms.Count);
+
+        }
+    }
+}
